562af8624fbd7b48653664ea0c930648d4058731
Cyril Jazra <>
2021-04-23 19:37:35 -0500

    added distributed training

---
+++ train_SOLO_model_distributed.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+OUTPUT_DIR=/home/cyriljazra/training_dir/SOLOv2_R50_3x
+export DETECTRON2_DATASETS=/home/cyriljazra/datasets/
+RANK=$1
+DIST_URL=$2
+PORT=7869
+echo "RANK: $RANK"
+echo "DIST URL: $DIST_URL"
+echo "CURRENT HOST: $HOSTNAME"
+/home/cyriljazra/AdelaiDet/tools/train_net.py --config-file configs/SOLOv2/R50_3x.yaml \
+	--num-machines 2 --machine-rank $RANK --dist-url $DIST_URL --num-gpus 4 --resume OUTPUT_DIR $OUTPUT_DIR
